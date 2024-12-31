281956e96207fcb6c6db5f64158b575d317c2186
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2024-12-31 12:18:11 +0530

    hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -55,42 +55,52 @@
         });
 </script>
 
-<h3 class="m-5 text-4xl font-medium text-gray-900 dark:text-white">Choose Products:</h3>
-<ul class="flex flex-wrap w-full gap-6 m-5">
-    {#each technologies as tech}
-        <li>
-            <input type="checkbox" id={tech.id} onclick={onclick} checked={ischeked(tech.id)} class="hidden peer" required="">
-            <label for={tech.id} class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-green-600 peer-checked:bg-green-500 peer-checked:text-white hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 hover:bg-gray-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">                           
-                <div class="block">
-                     <div class="w-full text-lg font-semibold">{tech.name}</div>
+<hero class="flex flex-col items-center justify-center md:h-screen w-full">
+    <h1 class="md:text-8xl sm:text-5xl text-4xl text-gray-900 dark:text-white my-5 font-bold">Elanngo Tex</h1>
+    <h1 class="md:text-6xl text-xl font-semibold text-gray-900 dark:text-white my-5">Welcomes You</h1>
+    <a href="tel:+919840930511" class="sm:text-2xl text-lg font-semibold text-center text-gray-600 dark:text-gray-300 my-5"><i class="ph-fill ph-phone mx-5"></i>9840930511</a>
+    <a href="https://maps.app.goo.gl/JCSeurhNWWVXzti8A" target="_blank" class="sm:text-2xl text-lg font-semibold text-center text-gray-600 dark:text-gray-300 my-5"><i class="ph-fill ph-map-pin-line mx-5"></i>Behind Eashwaran Kovil , Erode - 638001</a>
+</hero>
+<section class="w-full flex justify-center">
+    <div class="xl:w-3/4">
+        <h3 class="m-5 text-4xl font-medium text-gray-900 dark:text-white">Choose Products:</h3>
+        <ul class="flex flex-wrap justify-center gap-6 items-center">
+            {#each technologies as tech}
+                <li class="mx-2">
+                    <input type="checkbox" id={tech.id} onclick={onclick} checked={ischeked(tech.id)} class="hidden peer" required="">
+                    <label for={tech.id} class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-green-600 peer-checked:bg-green-500 peer-checked:text-white hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 hover:bg-gray-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">                           
+                        <div class="block">
+                             <div class="w-full text-lg font-semibold">{tech.name}</div>
+                        </div>
+                    </label>
+                </li>
+            {/each}
+        </ul>
+        <button id="clear-option" onclick={onclear} class="border-red-800 border-2 rounded-lg w-20 h-10 m-5 bg-red-700 text-white text-xl font-medium">Clear</button>
+        
+        <div class="mt-5 flex w-full flex-wrap justify-center items-center">
+            
+            {#if overlayVisible}
+                <button type="button" class="overlay" onclick={hideOverlay} onkeydown={(event) => event.key === 'Enter' && hideOverlay()} aria-label="Close overlay">
+                    <img src={overlayImage} alt="" class="scale-150"/>
+                </button>
+            {/if}
+        
+            {#each getSelectedTechnologies() as tech}
+                <div class="m-2">
+                    <h3 class="text-center text-5xl text-slate-800 font-medium">{tech.name}</h3>
+                    <div class="flex flex-wrap justify-center items-center">
+                        {#each tech.images as image}
+                            <button type="button" class="m-5 cursor-pointer" onclick={() => showOverlay(image)} onkeydown={(event) => event.key === 'Enter' && showOverlay(image)}>
+                                <img src={image} alt={tech.name} />
+                            </button>
+                        {/each}
+                    </div>
                 </div>
-            </label>
-        </li>
-    {/each}
-</ul>
-<button id="clear-option" onclick={onclear} class="border-red-800 border-2 rounded-lg w-20 h-10 m-5 bg-red-700 text-white text-xl font-medium">Clear</button>
-
-<div class="mt-5 flex w-full flex-wrap justify-center items-center">
-    
-    {#if overlayVisible}
-        <button type="button" class="overlay" onclick={hideOverlay} onkeydown={(event) => event.key === 'Enter' && hideOverlay()} aria-label="Close overlay">
-            <img src={overlayImage} alt="" class="scale-150"/>
-        </button>
-    {/if}
-
-    {#each getSelectedTechnologies() as tech}
-        <div class="m-2">
-            <h3 class="text-center text-5xl text-slate-800 font-medium">{tech.name}</h3>
-            <div class="flex flex-wrap justify-center items-center">
-                {#each tech.images as image}
-                    <button type="button" class="m-5 cursor-pointer" onclick={() => showOverlay(image)} onkeydown={(event) => event.key === 'Enter' && showOverlay(image)}>
-                        <img src={image} alt={tech.name} />
-                    </button>
-                {/each}
-            </div>
+            {/each}
         </div>
-    {/each}
-</div>
+    </div>
+</section>
 
 
 <style>
